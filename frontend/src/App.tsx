7ad4cf72ed7be8e1e461fbb49460f42e2f034b7b
Samudra-Bose <samudrabose7@gmail.com>
2026-09-21 03:07:51 +0530

    chore: remove CustomCursor animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -67,7 +67,6 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <CustomCursor />
       <Suspense fallback={<PageLoader />}>
         <Routes>
           {/* Public / Marketing */}
